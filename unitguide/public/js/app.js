dfc2b2adfc5e8275d895e3160847b71fb39a7333
Adam <adam@appsynergy.net>
2014-04-21 22:41:42 +0100

    Well, something regarding stat filtering...

--- unitguide/public/js/app.js
+++ unitguide/public/js/app.js
@@ -1,27 +1,44 @@
 var app = angular.module('unitguide', ['ngResource']);
 
-app.controller('MainCtrl', function($scope, $resource) {
+app.controller('MainCtrl', function($scope, $resource, $filter) {
 	
 	// fetch factories
 	$scope.factories = $resource('../data/Factories.json').get(function() {
-		// default to first factory (Cloaky)
-		$scope.selectedFactory = $scope.factories.data[0];
-	});
-	
-	// fetch units
-	$scope.units = $resource('../data/Units.json').get(function() {
-		//console.log($scope.units);
-	});
+		// fetch units
+		$scope.units = $resource('../data/Units.json').get(function() {
+			// default to first factory (Cloaky)
+			var fac = $scope.factories.data[0];
+			getFilterStats(fac.builds);
+			$scope.selectedFactory = fac;
+		});	
+	});	
 	
 	// choose a new factory
 	$scope.selectFactory = function() {
-		//console.log($scope.selectedFactory);
+		var builds = $scope.selectedFactory.builds;
+		getFilterStats(builds);
+	}
+	
+	// find the details of current units
+	function getFilterStats(builds) {
+		console.log(builds);
+		// would rather just do $filter(unitFilterByFac)
+		var units = [];
+		angular.forEach($scope.units.data, function(u) {
+			var makes = builds.indexOf(u.handle);
+			if (makes > 0) units.push(u);
+		});
+		angular.forEach(units, function(u) {
+			angular.forEach($scope.unitStats, function(s) {
+				//sriously?
+			});
+		});
 	}
 	
 	// used for filtering units list
 	$scope.unitFilterByFac = function(units) {
 		return function(u) {
-			// return true if in units
+			// return true if in the build list
 			var makes = units.indexOf(u.handle);
 			return makes > 0;
 		}
@@ -39,7 +56,7 @@ app.controller('MainCtrl', function($scope, $resource) {
 	// figure out a good width for the "strength indicator"
 	$scope.myWidth = function(key) {
 		var perc = (key/50 > 100) ? "100%" : (key/50)+"%";
-		console.log(key);
+		//console.log(key);
 		return perc;
 	}
 
